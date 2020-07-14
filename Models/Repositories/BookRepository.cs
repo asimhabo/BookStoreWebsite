f42f1446baab575c6d9d1aed3c5969e41916336b
asimhabo <aseemhaboowork@gmail.com>
2020-07-14 14:17:21 +0200

    Add project files.

---
+++ Models/Repositories/BookRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.Repositories
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        List<Book> books;
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id=1, Title="C# Programming", Description="None", Author = new Author()
+                },
+                new Book
+                {
+                    Id=2, Title="Java Programming", Description="No", Author = new Author()
+                },
+                new Book
+                {
+                    Id=3, Title="PHP Programming", Description="Null", Author = new Author()
+                }
+
+            };
+        }
+        public void Add(Book entity)
+        {
+            entity.Id = books.Max(b => b.Id) + 1;
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public List<Book> Search(string term)
+        {
+            var result = books.Where(b => b.Title.Contains(term)
+           || b.Description.Contains(term)
+           || b.Author.FullName.Contains(term)).ToList();
+
+            return result;
+        }
+
+        public void Update(int id ,Book newBook)
+        {
+            var book = Find(id);
+            book.Title = newBook.Title;
+            book.Description = newBook.Description;
+            book.Author = newBook.Author;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IBookstoreRepository<Author>, AuthorDbRepository>();
+            services.AddScoped<IBookstoreRepository<Book>, BookDbRepository>();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddDbContext<BookstoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlCon"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+           
+            
+
+        }
+    }
+}
